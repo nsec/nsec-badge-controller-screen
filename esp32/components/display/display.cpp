f10ac1c972a991410051e04fed35e3587432b730
EiNSTeiN- <einstein@g3nius.org>
2023-04-10 13:48:55 -0400

    Clean up display code, add beep when button is pressed

--- esp32/components/display/display.cpp
+++ esp32/components/display/display.cpp
@@ -23,6 +23,7 @@
 #include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
 
 #include "display.h"
+#include "buzzer.h"
 
 static const char *TAG = "display";
 #define LV_TICK_PERIOD_MS 10
@@ -44,70 +45,54 @@ static void lv_tick_task(void *arg) {
     lv_tick_inc(LV_TICK_PERIOD_MS);
 }
 
+
+/** Called when an action happened on the input device.
+ * The second parameter is the event from `lv_event_t`*/
+static void touch_feedback(struct _lv_indev_drv_t *, lv_event_t evt)
+{
+    switch(evt) {
+        case LV_EVENT_CLICKED: {
+            Buzzer::getInstance().beep();
+            break;
+        }
+    }
+}
+
 void Display::taskHandler()
 {
-    xGuiSemaphore = xSemaphoreCreateMutex();    // 创建GUI信号量
-    lv_init();          // 初始化LittlevGL
-    lvgl_driver_init(); // 初始化液晶SPI驱动 触摸芯片SPI/IIC驱动
-
     static lv_color_t buf1[DISP_BUF_SIZE];
-#ifndef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
     static lv_color_t buf2[DISP_BUF_SIZE];
-#endif
     static lv_disp_buf_t disp_buf;
-
     uint32_t size_in_px = DISP_BUF_SIZE;
 
-#if defined CONFIG_LVGL_TFT_DISPLAY_CONTROLLER_IL3820
-    /* Actual size in pixel, not bytes and use single buffer */
-    size_in_px *= 8;
-    lv_disp_buf_init(&disp_buf, buf1, NULL, size_in_px);
-#elif defined CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
-    lv_disp_buf_init(&disp_buf, buf1, NULL, size_in_px);
-#else
+    xGuiSemaphore = xSemaphoreCreateMutex();
+    lv_init();
+    lvgl_driver_init();
+
     lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
-#endif
 
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
-
-#ifdef CONFIG_LVGL_TFT_DISPLAY_MONOCHROME
-    disp_drv.rounder_cb = disp_driver_rounder;
-    disp_drv.set_px_cb = disp_driver_set_px;
-#endif
-
     disp_drv.buffer = &disp_buf;
     lv_disp_drv_register(&disp_drv);
 
-#if CONFIG_LVGL_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
     lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.read_cb = touch_driver_read;
+    indev_drv.feedback_cb = touch_feedback;
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     lv_indev_drv_register(&indev_drv);
-#endif
 
     const esp_timer_create_args_t periodic_timer_args = {
         .callback = &lv_tick_task,
-        .name = "periodic_gui"
+        .arg = NULL,
+        .name = "periodic_gui",
     };
     esp_timer_handle_t periodic_timer;
     ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &periodic_timer));
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
-    // 一个标签演示
-    //lv_obj_t * scr = lv_disp_get_scr_act(NULL);         // 获取当前屏幕
-    //lv_obj_t * label1 =  lv_label_create(scr, NULL);    // 在当前活动的屏幕上创建标签
-    //lv_label_set_text(label1, "Hello\nworld!");         // 修改标签的文字
-    // 对象对齐函数，将标签中心对齐，NULL表示在父级上对齐，当前父级是屏幕，0，0表示对齐后的x，y偏移量
-    //lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-/*
-	lv_indev_t * mouse_indev = lv_indev_drv_register(&indev_drv);
-	lv_obj_t * cursor_obj =  lv_img_create(lv_scr_act(), NULL); //Create an image object for the cursor
-	lv_img_set_src(cursor_obj, &mouse_cursor_icon);             //Set the image source
-	lv_indev_set_cursor(mouse_indev, cursor_obj);               //Connect the image  object to the driver
-*/
     while(true) {
 		if (xSemaphoreTake(xGuiSemaphore, (TickType_t)10) == pdTRUE) {
             lv_task_handler();
