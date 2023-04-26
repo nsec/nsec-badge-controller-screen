db028e19fd8d35a370b3eef2c02f44f56b5929fa
EiNSTeiN- <einstein@g3nius.org>
2023-04-26 15:22:30 -0400

    Enable reliable send flag for server commands

--- esp32/components/badge_mesh/badge/mesh/main.cpp
+++ esp32/components/badge_mesh/badge/mesh/main.cpp
@@ -11,8 +11,6 @@ static const char *TAG = "badge/mesh";
 
 TaskHandle_t BadgeMesh::_taskHandle = NULL;
 
-uint64_t _current_ping_idx = 0;
-
 void BadgeMesh::init()
 {
     networkTimeValid = false;
@@ -54,6 +52,7 @@ esp_err_t BadgeMesh::serverSend(uint16_t dst_addr, uint32_t op, uint8_t *msg, un
 		.net_idx = badge_network_info.net_idx,
 		.app_idx = badge_network_info.app_idx,
 		.addr = dst_addr,
+        .send_rel = 1,
 		.send_ttl = DEFAULT_TTL,
 	};
 
