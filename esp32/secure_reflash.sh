afc0dc1790f3468a3a5458a69940049f0afb1d0b
EiNSTeiN- <einstein@g3nius.org>
2023-06-22 10:33:32 -0400

    Update reflash script to flash bootloader

--- esp32/secure_reflash.sh
+++ esp32/secure_reflash.sh
@@ -1,6 +1,8 @@
 echo "** idf.py build is not run automatically, ensure files are up to date **"
 
+espsecure.py encrypt_flash_data --keyfile flash_encryption_key.bin --address 0x1000 --output build/bootloader/bootloader-encrypted.bin build/bootloader/bootloader.bin
 espsecure.py encrypt_flash_data --keyfile flash_encryption_key.bin --address 0x10000 --output build/partition_table/partition-table-encrypted.bin build/partition_table/partition-table.bin
 espsecure.py encrypt_flash_data --keyfile flash_encryption_key.bin --address 0x20000 --output build/nsec-badge-controller-screen-encrypted.bin build/nsec-badge-controller-screen.bin
 
-/home/einstein/.espressif/python_env/idf5.0_py3.10_env/bin/python ../esp-idf/components/esptool_py/esptool/esptool.py -p $1 -b 460800 --before default_reset --after no_reset --chip esp32  write_flash --force --flash_mode dio --flash_size keep --flash_freq 40m 0x10000 build/partition_table/partition-table-encrypted.bin 0x20000 build/nsec-badge-controller-screen-encrypted.bin
+/home/einstein/.espressif/python_env/idf5.0_py3.10_env/bin/python ../esp-idf/components/esptool_py/esptool/esptool.py -p $1 -b 460800 --before default_reset --after no_reset --chip esp32 \
+	 write_flash --force --flash_mode dio --flash_size keep --flash_freq 40m 0x1000 build/bootloader/bootloader-encrypted.bin 0x10000 build/partition_table/partition-table-encrypted.bin 0x20000 build/nsec-badge-controller-screen-encrypted.bin
